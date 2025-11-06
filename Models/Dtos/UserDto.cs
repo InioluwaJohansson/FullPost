40a37cd644dd2b286516616c76790991f8aca616
Inioluwa Johansson <inioluwa.makinde10@gmail.com>
2025-11-06 02:20:18 +0000

    Adding Email Services

---
+++ Models/Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+namespace FullPost.Models.DTOs;
+
+public class LoginResponse : BaseResponse
+{
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+}
