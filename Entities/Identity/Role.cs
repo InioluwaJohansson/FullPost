a89e41e8222dc08ec9febd11356336e03ececc1b
Inioluwa Johansson <inioluwa.makinde10@gmail.com>
2025-11-08 15:37:38 +0000

    Removed unused files.

--- Entities/Identity/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿using FullPost.Contracts;
-
-namespace FullPost.Entities.Identity;
-
-public class Role : AuditableEntity
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-}
